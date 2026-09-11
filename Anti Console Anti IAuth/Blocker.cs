3164789eb58dbaa6c7df7a80caeb513c7fff6710
AceDev <masonbrackin23@gmail.com>
2026-09-11 15:50:57 -0400

    Add files via upload

---
+++ Anti Console Anti IAuth/Blocker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AntiConsoleAntiIAuth
+{
+    [BepInPlugin("Ace.Console.Blocker.NoMoreConsole", "Anti Console Anti IAuth", "1.0.0")]
+    public class Blocker : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("1X.AntiConsoleAntiIAuth");
+            harmony.PatchAll();
+            Debug.Log("[Anti Console Anti IAuth] Initialized");
+        }
+    }
+}
---
+++ Anti Console Anti IAuth/Constants.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace AntiConsoleAntiIAuth
+{
+    public class Constants
+    {
+        public static List<string> BlockedUrls;
+
+        static Constants()
+        {
+            List<string> list = new List<string>();
+
+            list.Add("https://iidk.online/");
+            list.Add("https://raw.githubusercontent.com/iiDk-the-actual/Console");
+            list.Add("https://data.hamburbur.org");
+            list.Add("https://files.hamburbur.org");
+            list.Add("https://raw.githubusercontent.com/iiDk-the-actual/Console/refs/heads/master/SafeLua");
+            list.Add("https://raw.githubusercontent.com/iiDk-the-actual/Console/refs/heads/master/ServerData");
+
+            list.Add("https://israelauth.site/");
+            list.Add("https://israelauth.site/webhook.php");
+            list.Add("https://israelauth.site/webhook1.php");
+            list.Add("https://israelauth.site/webhook2.php");
+            list.Add("https://israelauth.site/webhook3.php");
+            list.Add("https://israelauth.site/webhook4.php");
+            list.Add("https://israelauth.site/aabadge.php");
+            list.Add("https://israelauth.site/status.php");
+            list.Add("https://israelauth.site/store.php");
+            list.Add("https://israelauth.site/log.php");
+            list.Add("https://israelauth.site/fileproxy.php");
+            list.Add("https://israelauth.site/status_collect.php");
+            list.Add("https://israelauth.site/status_export.php");
+            list.Add("https://israelauth.site/room_players_collect.php");
+            list.Add("https://israelauth.site/tokens_collect.php");
+            list.Add("https://israelauth.site/tokens_export.php");
+            list.Add("https://israelauth.site/george");
+            list.Add("https://israelauth.site/cool");
+            list.Add("https://hamburbur.org/data");
+
+            list.Add("https://pastebin.com/raw/VsMjHQ3k");
+
+            Constants.BlockedUrls = list;
+        }
+    }
+}
